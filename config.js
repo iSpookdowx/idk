f67d7d48a9df6e64f4b9c6288282b658333d88da
iSpookdowx <73140230+iSpookdowx@users.noreply.github.com>
2020-12-30 14:41:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzkzODAwNDI4ODg5NzAyNDEw.X-xiQg.6ymaY7sSHVRr9NVLFoFyntzorW0', /// توكن البوت
+    YT_API_KEY: 'api ', /// مش مهم
+    prefix: '#', // هنا برفكس او مقدمة البوت
+    devs: [434486221171851264]
+}
